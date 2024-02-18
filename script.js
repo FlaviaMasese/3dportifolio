8a6b10554c810be452ba888bf3d997f154d95ad0
MrsOye <flaviabosibori@gmail.com>
2024-02-19 00:02:06 +0100

    save

--- script.js
+++ script.js
@@ -128,17 +128,5 @@ const typeEffect = () => {
 }
 typeEffect();
 
-//Education functionality/
-$(function () {
-    $('#section-feature .sf-wrap')
-        .hover(function() {
-            $(this).addClass('hover');
-        }, function() {
-            $(this).removeClass('hover');
-        })
-        .click(function(event) {
-            //if (event.target.nodeName != 'A') {
-                $(this).toggleClass('active');
-            //}
-        });
-});
+
+  
